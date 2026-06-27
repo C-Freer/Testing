d89e0573f1cc787a7cdbee1ae56419d8f9112a4f
C.Freer <cafr3872@bht-berlin.de>
2026-06-27 13:42:48 +0200

    A2: GREEN: allow adding single item to shopping cart

--- src/main/java/com/example/ShoppingCart.java
+++ src/main/java/com/example/ShoppingCart.java
@@ -4,10 +4,18 @@ package com.example;
  * Simple shopping cart for TDD exercise
  */
 
+
 public class ShoppingCart {
 
+    private int total = 0;
+
+    public void addItem(int price) {
+        total = price;
+    }
+
     public int total() {
-        return 0;
+        return total;
     }
 }
+
 
