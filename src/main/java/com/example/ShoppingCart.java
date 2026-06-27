f588b03a17e3a17ec66b1140ee97fffb25383df1
C.Freer <cafr3872@bht-berlin.de>
2026-06-27 13:47:53 +0200

    A2: REFACTOR: correct accumulation of item prices

--- src/main/java/com/example/ShoppingCart.java
+++ src/main/java/com/example/ShoppingCart.java
@@ -4,13 +4,12 @@ package com.example;
  * Simple shopping cart for TDD exercise
  */
 
-
 public class ShoppingCart {
 
     private int total = 0;
 
     public void addItem(int price) {
-        total = price;
+        total += price;
     }
 
     public int total() {
